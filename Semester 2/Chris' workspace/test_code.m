e53d19ae355bef5c045d734f0288382a5529dfb1
ChrisMW117 <147422914+ChrisMW117@users.noreply.github.com>
2024-02-29 14:42:15 +0000

    Add files via upload

--- Semester 2/Chris' workspace/test_code.m
+++ Semester 2/Chris' workspace/test_code.m
@@ -1,43 +1,120 @@
 clear all, close all
 clc
+
 %% Constants
 
-N = 1024;
-c = 299792458;    %Speed of light
-Zmax = 0.03;      %length of waveguide
+N=1024;
+c = 299792458; %Speed of light
+Zmax = 0.02; %length of waveguide in m
 
-load("photon_disp.mat"); lscan_photon = lamscan; neff_photon=neff;   %Data for i and s pulses
-load("pump_disp.mat"); lscan_pump = lamscan; neff_pump=neff;         %Data for pump pulse
-lscan_pump=lscan_pump*10^-6;                                         %Converting from um to m
+load("photon_disp.mat"); %Data for i and s pulses
+lscan_photon = lamscan; neff_photon=neff;   
+lscan_photon=lscan_photon*10^-6;   %Converting from um to m
+lambda_min = min(lscan_photon); lambda_max = max(lscan_photon);
 
-%% Frequencies 
-% Generate array of frequencies, wavelegnths and n_effectives
-lambda_min = 1.35e-6; lambda_max = 1.65e-6; % Minimum and maximum wavelength in meters
+load("pump_disp.mat"); lscan_pump = lamscan; neff_pump=neff; %Data for pump pulse
+lscan_pump=lscan_pump*10^-6;   %Converting from um to m
+
+%% 
+
 omega_max = 2*pi*c/lambda_min; omega_min = 2*pi*c/lambda_max;
+wi = linspace(omega_min, omega_max, 2000); 
+ws = linspace(omega_min, omega_max, 2000);
+wp_0 = wi + ws;
 
-n_min = min(neff_photon); n_max = max(neff_photon);
+wscan_photon=2*pi*c./(lscan_photon);
 
-%diff array lengths to ensure proper size multiplication
-wi = linspace(omega_min, omega_max, 1000); ws = linspace(omega_min, omega_max, 1020);
-ni = linspace(n_max, n_min, 1000); ns = linspace(n_max, n_min, 1020);
+ns=spline(wscan_photon,neff_photon,ws);  %using spline to get range of neff that follow same relation between neff_photon and wscan_photon
+ni=spline(wscan_photon,neff_photon,wi);  
 
-[Ws,Wi] = meshgrid(ws,wi); [Ns, Ni] = meshgrid(ns, ni);   %Converting arrays to meshgrids 
-Wp = Ws + Wi;                                             %freq_p = freq_s + freq_i
+wscan_pump=2*pi*c./(lscan_pump); 
 
-fitType = fittype('a/x');                                             %n=lambda_0/lambda
-params = fit(lscan_pump, neff_pump, fitType, StartPoint = [1.5e-6]);  %fitting pump data to above equation
-lambda_0 = params(1);                                                 %defining lambda_0
+[Ws,Wi] = meshgrid(ws,wi);     %Converting arrays to meshgrids
+[Ns, Ni] = meshgrid(ns, ni);   %Ws vertical, Wi horizontal
+Wp = Ws + Wi;                  %freq_p = freq_s + freq_i
 
-Np = lambda_0*Wp./(2*pi*c);    %creating meshgrid of n_eff for pump pulse
+Np = spline(wscan_pump,neff_pump,Wp);   %using spline to get range of neff that follow same relation between neff_pump and wscan_pump
 
 %% Betas
 
-Beta_s = Ws.*Ns./c; Beta_i = Wi.*Ni./c; Beta_p = Wp.*Np./c;   %calculating beta values for all 3 pulses
-delta_beta = Beta_p - Beta_s - Beta_i;                        %calculating delta_beta
+Beta_s = Ws.*Ns./c;   %beta = n*w/c
+Beta_i = Wi.*Ni./c; 
+Beta_p = Wp.*Np./c;   %calculating beta values for all 3 pulses
 
-phi = sinc(delta_beta, Zmax);     %phi = sinc(delta_beta*Z/2)
+delta_beta = Beta_p - Beta_s - Beta_i;  %delta_beta = beta_p - beta_s - beta_i
+phi = sinc(delta_beta, Zmax);
+
+w0=2*pi*c/750e-9;
+alpha=exp(-(Ws+Wi-w0).^2*0.25e-24/2);
 
 %% Plotting
 
-pcolor(Ws, Wi, phi)
+figure
+pcolor(Ws,Wi,alpha);
+shading interp;
+xlabel('\omega_s (Hz)');
+ylabel('\omega_i (Hz)');
+title('\alpha');
+colorbar;
+%caxis([0 1]);
+set(gca,'TickDir','out'); 
+
+figure
+pcolor(Ws,Wi,phi);
+shading interp;
+xlabel('\omega_s (Hz)');
+ylabel('\omega_i (Hz)');
+title('\phi');
+colorbar;
+%caxis([0 1]);
+set(gca,'TickDir','out'); 
+
+figure
+pcolor(Ws,Wi,alpha.*phi);
+shading interp;
+xlabel('\omega_s (Hz)');
+ylabel('\omega_i (Hz)');
+title('\phi \alpha');
+colorbar;
+%caxis([0 1]);
+set(gca,'TickDir','out'); 
+
+figure
+pcolor(Ws,Wi,alpha.*phi);
+shading interp;
+xlabel('\omega_s (Hz)');
+ylabel('\omega_i (Hz)');
+title('Closeup 1');
+xlim([1.148e15 1.165e15])
+ylim([1.34e15 1.37e15])
+colorbar;
+%caxis([0 1]);
+set(gca,'TickDir','out'); 
+
+figure
+pcolor(Ws,Wi,alpha.*phi);
+shading interp;
+xlabel('\omega_s (Hz)');
+ylabel('\omega_i (Hz)');
+title('Closeup 2');
+xlim([1.34e15 1.37e15])
+ylim([1.148e15 1.165e15])
+colorbar;
+%caxis([0 1]);
+set(gca,'TickDir','out'); 
+
+dz = Zmax/N;
+
+trap = alpha.*0.5 * alpha.*exp(1i*delta_beta.*Zmax).*0.5;
+
+for c=1:N-1
+    trap = trap + alpha.*exp(1i*delta_beta.*dz*c)*1;
+end
+
+figure
+pcolor(Ws,Wi, abs(trap));
+shading interp;
+xlabel('\omega_s (Hz)');
+ylabel('\omega_i (Hz)');
+title('Integral');
 colorbar
